04b316f7e6abe45f6ffb4ff2b77dea5080cd1a9c
Egor <georgebazhenov27@gmail.com>
2022-05-08 23:10:17 +0300

    camera

--- src/script.js
+++ src/script.js
@@ -43,23 +43,14 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 1
+camera.position.z = 10
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Cube
- */
-// const geometry = new THREE.SphereGeometry(1, 32, 32)
-//
-// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-//
-// const tunnel =  new THREE.Mesh(geometry, material)
-//
-// scene.add(tunnel)
+
 
 class CustomSinCurve extends THREE.Curve {
 	constructor( scale = 1 ) {
@@ -80,7 +71,8 @@ class CustomSinCurve extends THREE.Curve {
 }
 
 const path = new CustomSinCurve( 10 );
-const tubeGeometry = new THREE.TubeGeometry( path, 200, 1, 32, false );
+const tubeGeometry = new THREE.TubeGeometry( path, 200, 1, 64, false );
+
 
 const material = new THREE.MeshBasicMaterial( {
   side: THREE.DoubleSide,
@@ -91,6 +83,7 @@ material.map.wrapT = THREE.RepeatWrapping;
 material.map.repeat.set(10, 1)
 
 const mesh = new THREE.Mesh( tubeGeometry, material );
+// mesh.rotation.z = Math.PI / 2
 scene.add( mesh );
 
 /**
@@ -106,61 +99,53 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-//let lastElapsedTime = 0
+
 let normal = new THREE.Vector3()
 let binormal = new THREE.Vector3()
-let lookAt = new THREE.Vector3()
-
 
 const tick = () =>
 {
-        // const elapsedTime = clock.getElapsedTime()
-        // const deltaTime = elapsedTime - lastElapsedTime
-        // lastElapsedTime = elapsedTime
+  let time = Date.now()
+  let looptime = 60 * 1000
+  let t = (time % looptime) / looptime
+  let pos = tubeGeometry.parameters.path.getPointAt( t )
+  
+  tubeGeometry.parameters.path.getPointAt( t );
 
-        const time = Date.now();
-    		const looptime = 20 * 1000;
-    		const t = ( time % looptime ) / looptime;
+	const segments = tubeGeometry.tangents.length;
+	const pickt = t * segments;
+	const pick = Math.floor( pickt );
+	const pickNext = ( pick + 1 ) % segments;
 
-        let pos = tubeGeometry.parameters.path.getPointAt( t );
+	binormal.subVectors( tubeGeometry.binormals[ pickNext ], tubeGeometry.binormals[ pick ] );
+	binormal.multiplyScalar( pickt - pick ).add( tubeGeometry.binormals[ pick ] );
 
-				// interpolation
-        const segments = tubeGeometry.tangents.length;
-				const pickt = t * segments;
-				const pick = Math.floor( pickt );
-				const pickNext = ( pick + 1 ) % segments;
+	const dir = tubeGeometry.parameters.path.getTangentAt( t );
+	const offset = 0;
 
-				binormal.subVectors( tubeGeometry.binormals[ pickNext ], tubeGeometry.binormals[ pick ] );
-				binormal.multiplyScalar( pickt - pick ).add( tubeGeometry.binormals[ pick ] );
-
-				const direction = tubeGeometry.parameters.path.getTangentAt( t );
-				const offset = 0;
-
-				normal.copy( binormal ).cross( direction );
+	normal.copy( binormal ).cross( dir );
 
 				// we move on a offset on its binormal
 
-				pos.add( normal.clone().multiplyScalar( offset ) );
+	pos.add( normal.clone().multiplyScalar( offset ) );
 
-				// splineCamera.position.copy( pos );
-				camera.position.copy( pos );
+	camera.position.copy( pos );
 
-        tubeGeometry.parameters.path.getPointAt((t + 1 / tubeGeometry.parameters.path.getLength()) % 1, lookAt)
 				// using arclength for stablization in look ahead
 
-				camera.matrix.lookAt( camera.position, lookAt, normal );
-				camera.rotation.setFromRotationMatrix( camera.matrix, camera.rotation.order );
+	let lookAt = tubeGeometry.parameters.path.getPointAt( ( t + 30 / tubeGeometry.parameters.path.getLength() ));
 
+  camera.matrix.lookAt( camera.position, lookAt, normal );
+  camera.rotation.setFromRotationMatrix( camera.matrix, camera.rotation.order );
+  
 
-        // Update controls
-        controls.update()
+  controls.update()
 
-        // Render
-        renderer.render(scene, camera)
-
-        // Call tick again on the next frame
-        window.requestAnimationFrame(tick)
+  // Render
+  renderer.render(scene, camera)
+  
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
